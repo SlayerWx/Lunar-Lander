a6f1e78c31578e807f03d25bc8954607d1afed17
Maxi Ruffo <maxi_cs_95@hotmail.com>
2020-07-05 05:52:17 -0300

    splashSCreen

---
+++ Assets/SplashScreen script/SplashScreen.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class SplashScreen : MonoBehaviour
+{
+    [SerializeField]
+    private float timer;
+    [SerializeField]
+    private GameObject empresa;
+    [SerializeField]
+    private GameObject juego;
+    void Start()
+    {
+        StartCoroutine(Sequence());
+    }
+    IEnumerator Sequence()
+    {
+        empresa.SetActive(true);
+        juego.SetActive(false);
+        yield return new WaitForSeconds(timer);
+        empresa.SetActive(false);
+        juego.SetActive(true);
+        yield return new WaitForSeconds(timer);
+        SceneManager.LoadScene("Menu", LoadSceneMode.Single);
+
+
+    }
+}
