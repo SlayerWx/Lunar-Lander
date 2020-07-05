6db31b0e7eabdeb1e454327c4f30479131d34218
Maxi Ruffo <maxi_cs_95@hotmail.com>
2020-07-05 10:14:36 -0300

    Menu Basico Completo

---
+++ Assets/Menu/MenuManager.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class MenuManager : MonoBehaviour
+{
+    [SerializeField]
+    private GameObject mainLayer;
+    [SerializeField]
+    private GameObject creditsLayer;
+
+    public void Play()
+    {
+        SceneManager.LoadScene("Game", LoadSceneMode.Single);
+    }
+    public void CreditsEntry()
+    {
+        creditsLayer.SetActive(true);
+        mainLayer.SetActive(false);
+    }
+    public void CreditsDeparture()
+    {
+        mainLayer.SetActive(true);
+        creditsLayer.SetActive(false);
+    }
+    public void ExitGame()
+    {
+        Application.Quit();
+    }
+}
