ceba167f1a720032a142c82e8fab19cb62f05449
Maxi Ruffo <maxi_cs_95@hotmail.com>
2020-07-09 14:59:00 -0300

    renombrando myCollision a ObjectiveCollision

--- Assets/Script/Map/ObjectiveCollision.cs
+++ Assets/Script/Map/ObjectiveCollision.cs
@@ -2,7 +2,7 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class myCollision : MonoBehaviour
+public class ObjectiveCollision : MonoBehaviour
 {
     [SerializeField]
     bool imVictoryCollision = false;
